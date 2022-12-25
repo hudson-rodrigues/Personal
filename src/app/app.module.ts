7c32ad384378716f034de6a06632d528872d8417
Hudson Rodrigues <hudson_rs@outlook.com>
2022-12-25 13:36:01 -0300

    iniciando teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
